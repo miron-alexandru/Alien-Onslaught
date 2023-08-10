7b9fcb584df24ec36cb5017c3ea80ace582bab40
Ake <khadaake@gmail.com>
2023-08-10 20:02:11 +0300

    ship_selection_manager refactored for better maintainability.

--- src/managers/player_managers/ship_selection_manager.py
+++ src/managers/player_managers/ship_selection_manager.py
@@ -13,13 +13,15 @@ class ShipSelection:
     """The ShipSelection class handles the ship selection in the game."""
 
     def __init__(self, game, screen, ship_images, settings):
-        self.screen = screen
-        self.settings = settings
         self.game = game
+        self.screen = screen
+        self.ship_images = ship_images
+        self.settings = settings
+
         self.thunderbird_ship = self.game.thunderbird_ship
         self.phoenix_ship = self.game.phoenix_ship
+
         self.font = pygame.font.SysFont("verdana", 26)
-        self.ship_images = ship_images
         self.clickable_regions = []
 
         self.ship_selection_functions = {
@@ -72,63 +74,95 @@ class ShipSelection:
         screen_width = self.screen.get_width()
         self.clickable_regions = []
 
-        self.draw_ship_type_text(1, 25, screen_width * 0.05)
-        self.draw_ships(1, 25, screen_width * 0.05)
-
+        self.draw_ships_and_text(1, 25, screen_width * 0.05)
+        
         if not self.game.singleplayer:
-            self.draw_ship_type_text(2, 25, screen_width * 0.3)
-            self.draw_ships(2, 25, screen_width * 0.3)
+            self.draw_ships_and_text(2, 25, screen_width * 0.3)
+    
+    def draw_ships_and_text(self, ship_type, y_offset, x_position):
+        """Draw ship type text and ships for a given player."""
+        self.draw_ship_type_text(ship_type, y_offset, x_position)
+        self.draw_ships(ship_type, y_offset, x_position)
 
     def draw_ship_type_text(self, ship_type, y_position, x_position):
         """Draw the ship type text on the screen."""
         ship_type_text = "Thunderbird" if ship_type == 1 else "Phoenix"
-        ship_type_text_color = (255, 255, 255)
-        text_surface = self.font.render(ship_type_text, True, ship_type_text_color)
+        text_surface = self.font.render(ship_type_text, True, (255, 255, 255))
         self.screen.blit(text_surface, (x_position, y_position))
 
     def draw_ships(self, ship_type, y_position, x_position):
         """Draws the ship images on the screen."""
-        ship_images = self.ship_images[(ship_type - 1) * 3 : ship_type * 3]
+        ship_images = self.get_ship_images(ship_type)
         image_width = ship_images[0].get_width()
+        spacing = 15
 
         for i, ship_image in enumerate(ship_images):
-            x_pos = x_position + (i * (image_width + 20))
-            ship_rect = ship_image.get_rect()
-            ship_rect.topleft = (x_pos, y_position + 50)
-            self.screen.blit(ship_image, ship_rect)
-            self.clickable_regions.append((ship_rect, ship_type, i))
+            x_pos = x_position + (i * (image_width + spacing))
+            ship_rect = self.position_ship(ship_image, x_pos, y_position + 50)
+            self.register_clickable_region(ship_rect, ship_type, i)
+            self.display_ship_description(ship_rect, ship_type, i)
 
-            if ship_rect.collidepoint(pygame.mouse.get_pos()):
-                if ship_type == 1:
-                    display_description(
-                        self.screen, THUNDER_SHIP_DESCRIPTIONS[i], 55, 150
-                    )
-                else:
-                    display_description(
-                        self.screen, PHOENIX_SHIP_DESCRIPTIONS[i], 55, 150
-                    )
+    def get_ship_images(self, ship_type):
+        """Get ship images based on the ship type."""
+        start_index = (ship_type - 1) * 3
+        end_index = ship_type * 3
+        return self.ship_images[start_index:end_index]
+
+    def position_ship(self, ship_image, x_pos, y_pos):
+        """Position a ship image and return its rect."""
+        ship_rect = ship_image.get_rect(topleft=(x_pos, y_pos))
+        self.screen.blit(ship_image, ship_rect)
+        return ship_rect
+
+    def register_clickable_region(self, ship_rect, ship_type, index):
+        """Register a clickable region for a ship."""
+        self.clickable_regions.append((ship_rect, ship_type, index))
+
+    def display_ship_description(self, ship_rect, ship_type, index):
+        """Display ship description when hovered over."""
+        descriptions = (
+            THUNDER_SHIP_DESCRIPTIONS if ship_type == 1 else PHOENIX_SHIP_DESCRIPTIONS
+        )
+        if ship_rect.collidepoint(pygame.mouse.get_pos()):
+            display_description(self.screen, descriptions[index], 60, 140)
 
     def handle_ship_selection(self, mouse_pos):
         """Handles the ship selection."""
         for region, ship_type, index in self.clickable_regions:
-            if (
-                region.collidepoint(mouse_pos)
-                and self.game.ui_options.ship_selection
-                and (ship_type, index) in self.ship_selection_functions
-            ):
-                ship_name, ship_function = self.ship_selection_functions[
-                    (ship_type, index)
-                ]
-                if ship_type == 1:
-                    self.thunderbird_ship.ship_name = ship_name
-                    self.thunderbird_ship.ship_selected = True
-                elif ship_type == 2:
-                    self.phoenix_ship.ship_name = ship_name
-                    self.phoenix_ship.ship_selected = True
-                ship_function()
+            if self.is_valid_ship_selection(region, ship_type, index, mouse_pos):
+                self.select_ship(ship_type, index)
                 play_sound(self.game.sound_manager.game_sounds, "select_ship")
+        
+        self.update_ship_selection_state()
 
+    def is_valid_ship_selection(self, region, ship_type, index, mouse_pos):
+        """Checks if the ship selection is valid."""
+        return (
+            region.collidepoint(mouse_pos)
+            and self.game.ui_options.ship_selection
+            and (ship_type, index) in self.ship_selection_functions
+        )
+
+    def select_ship(self, ship_type, index):
+        """Selects the chosen ship and performs the associated function."""
+        ship_name, ship_function = self.ship_selection_functions[(ship_type, index)]
+        if selected_ship := self.get_selected_ship(ship_type):
+            selected_ship.ship_name = ship_name
+            selected_ship.ship_selected = True
+        ship_function()
+
+    def get_selected_ship(self, ship_type):
+        """Returns the selected ship instance based on the ship type."""
+        ship_mapping = {
+            1: self.thunderbird_ship,
+            2: self.phoenix_ship
+        }
+        return ship_mapping.get(ship_type)
+
+    def update_ship_selection_state(self):
+        """Update ship selection state based on conditions."""
         if self.game.singleplayer and self.thunderbird_ship.ship_selected:
             self.game.ui_options.ship_selection = False
         elif self.phoenix_ship.ship_selected and self.thunderbird_ship.ship_selected:
             self.game.ui_options.ship_selection = False
+
